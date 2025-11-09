a4fcdd5a740fd8286d903ee01cbc3d7c31538c17
Jareth20 <jarethrojas166@gmail.com>
2025-11-09 05:11:40 +0000

    Agregar archicos ArrayList,DoublyNodeList,DoublyLinkedList,CircularNodeList y CircularLinkedList

---
+++ EcoTrack/src/main/java/espol/grupo_11/ecotrack/CircularNodeList.java
@@ -0,0 +1,28 @@
+package espol.grupo_11.ecotrack;
+
+public class CircularNodeList<E> {
+    private E content;
+    private CircularNodeList<E> next;
+    
+    public CircularNodeList(E content){
+        this.content=content;
+        this.next=this; // Instancia de un nodo el next ya no apunta a null sino a si mismo
+    }
+    
+    public E getContent(){
+        return content;
+    }
+    
+    public void setContent(E content){
+        this.content = content;
+    }
+    
+    public CircularNodeList<E> getNext(){
+        return next;
+    }
+    
+    public void setNext(CircularNodeList<E> next){
+        this.next = next;
+    }
+}
+
