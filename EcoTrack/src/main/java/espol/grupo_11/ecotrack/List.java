a40c3ad6e02ae2a084688f48339a8943a43269b5
Jareth20 <jarethrojas166@gmail.com>
2025-11-09 04:30:45 +0000

    Implementación de la clase LinkedList y NodeList para la estructura de datos de lista enlazada

--- EcoTrack/src/main/java/espol/grupo_11/ecotrack/List.java
+++ EcoTrack/src/main/java/espol/grupo_11/ecotrack/List.java
@@ -1,18 +1,26 @@
 package espol.grupo_11.ecotrack;
 
-public interface List<E> {
-    
-    public int size();  
-    public boolean isEmpty(); 
-    public void clear();
-    public boolean addFirst(E e);
-    public boolean addLast(E e);
-    public E removeFirst();
-    public E removeLast();
-    public void add (int index, E element);
-    public E remove (int index);
-    public E get (int index); 
-    public E set (int index, E element);
-    public String toString();
+public interface List<E> extends Iterable<E>{
 
+    public boolean addFirst(E e); //Inserta el elemento e al inicio
+            
+    public boolean addLast(E e); //Inserta el elemento e al final
+            
+    public E removeFirst(); //remueve el elemento al inicio de la lista
+    
+    public E removeLast(); // remueve el elemento al final de la lista
+    
+    public int size();
+    
+    public boolean isEmpty();
+    
+    public void clear();
+            
+    public void add(int index, E element); //Inserta el elemento en la posicion index
+            
+    public E remove(int index); // remueve y retorna el elemento en la posicion index
+    
+    public E get(int index); //retorna el elemento ubicado en la posicion index
+    
+    public E set(int index, E element); //setea elemento en la posicion index
 }
