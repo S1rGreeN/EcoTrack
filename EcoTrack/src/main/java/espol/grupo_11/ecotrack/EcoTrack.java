23fa7212eee3bcce530f33f139e97c06bbab9cf2
Alejandro Verdesoto <s1rgreen.dev@gmail.com>
2025-11-08 21:21:44 -0500

    Estructura Netbeans
    base

---
+++ EcoTrack/src/main/java/espol/grupo_11/ecotrack/EcoTrack.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espol.grupo_11.ecotrack;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EcoTrack {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
