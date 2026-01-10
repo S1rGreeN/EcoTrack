914690d6a6ee6924556e6fe55f97d3d42d58f162
Alejandro Verdesoto <s1rgreen.dev@gmail.com>
2026-01-10 07:09:45 +0000

    Revisar clases CircularDoubleLinkedListy zona, falta implementar la clase de PriorityQueue con comparador personalizado de Funcion Utilidad Ecotrack

--- EcoTrack/src/main/java/espol/grupo_11/ecotrack/Modelo/Zona.java
+++ EcoTrack/src/main/java/espol/grupo_11/ecotrack/Modelo/Zona.java
@@ -1,18 +1,41 @@
 package espol.grupo_11.ecotrack.Modelo;
 
 import espol.grupo_11.ecotrack.Utilitarios.CircularDoubleLinkedList;
+import java.util.Iterator;
 
 public class Zona {
-    CircularDoubleLinkedList listaResiduos;
+    CircularDoubleLinkedList<Residuo> listaResiduos;
+    String codigo; //Definir si usamos el codigo postal de la parroquia o el codigo postal de cada zona de guayaquil.
+    int cantidadResiduosRecolectados; 
+    /*Definir si se usa por dias con hilos con varios procesos de recoleccion o si 
+    se reinicia por semanas con hilos(lo veo viable, es lo mas realista, 
+    ya que un camion de basura pasa por toda la ciudad en la semana).  */
 
-    public Zona(CircularDoubleLinkedList listaResiduos){
+    public Zona(CircularDoubleLinkedList<Residuo> listaResiduos, String codigo){
         this.listaResiduos = listaResiduos;
+        this.codigo = codigo;
     }
+    public CircularDoubleLinkedList<Residuo> getListaResiduos(){return listaResiduos;}
+    public String getCodigo(){return codigo;}
 
+
+    public boolean botarResiduo(Residuo nuevoResiduo){
+        if(nuevoResiduo == null){return false;}
+        listaResiduos.addLast(nuevoResiduo);
+        return true;
+    }
     public int getPrioridadAmbiental(){
         int contador = 0;
-        //Codigo
-
+        Iterator<T> it = listaResiduos.iterator();
+        if(!it.hasNext()){
+            return 0;
+        }
+        Residuo prev = it.next();
+        contador += prev.getPrioridadAmbiental();
+        while(!it.hasNext()){
+            Residuo current = it.next();
+            contador += current.getPrioridadAmbiental();
+        }
         return contador;
     }
 }
--- EcoTrack/src/main/java/espol/grupo_11/ecotrack/Utilitarios/CircularDoubleLinkedList.java
+++ EcoTrack/src/main/java/espol/grupo_11/ecotrack/Utilitarios/CircularDoubleLinkedList.java
@@ -1,5 +1,77 @@
 package espol.grupo_11.ecotrack.Utilitarios;
+import java.util.Iterator;
 
-public class CircularDoubleLinkedList {
+public class CircularDoubleLinkedList<T> implements Iterable<T> {
+    private DoublyNodeList<T> head; 
+    public CircularDoubleLinkedList(){
+        this.head = null;
+    }
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    public int size(){
+        if(isEmpty()){return 0;}
+        int contador = 0;
+        DoublyNodeList<T> current;
+        do{
+            contador++;
+            current = current.getNext();
+        } while(curreant!=head);
+        return contador;
+    }
+
+    public boolean addLast(T content){
+        if(content == null){ throw new IllegalArgumentException();}
+        DoublyNodeList<T> nuevoNodo = new DoublyNodeList<>(content);
+        if(isEmpty()){
+            head = nuevoNodo;
+            head.setPrevious(nuevoNodo);
+            head.setNext(nuevoNodo);
+            return true;
+        } else {
+            DoublyNodeList<T> last = head.getPrevious();
+            nuevoNodo.setNext(head);
+            nuevoNodo.setPrevious(last);
+            last.setNext(nuevoNodo);
+            head.setPrevious(nuevoNodo);
+            return true;
+        }
+    }
+
+
+    public boolean addFirst(T content){
+        if(content == null){ throw new IllegalArgumentException();}
+        addLast(content);
+        head = head.getPrevious();
+        return true;
+    }
+
+    public boolean remove(T content){
+        if (isEmpty()) {return false;}
+
+        Node<T> current = head;
+        do {
+            if (current.getContent().equals(content)) {
+                if (current.getNext() == current) {
+                    head = null;
+                } else {
+                    Node<T> prevNode = current.getPrevious();
+                    Node<T> nextNode = current.getNext();
+                    prevNode.setNext(nextNode);
+                    nextNode.setPrevious(prevNode);
+                    if (current == head) {
+                        head = nextNode;
+                    }
+                }
+                return true;
+            }
+            current = current.getNext();
+        } while (current != head);
+
+        return false;
+    }
+    //Definir si agregamos los metodos remove y add por indices
 
 }
